c4f91bc345ab65cf06181919b14147cfc2bbc59f
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-08-09 20:58:59 -0400

    updated js and html for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-// Need FOrm INputs  ==> HTML FORM with input (text) and it needs an id
+// Need Form Input  ==> HTML FORM with input (text) and it needs an id
 // KNow What the value of the input is in JS (querySelector for the ID)
 
 //How would I know in JS when the user searches? EventListener on Click for another element and it also needs an id
@@ -14,44 +13,177 @@ let weather = {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        "&units=metric&appid=c27c727042da1e145bf14c1d47031e5d"
+        "&units=metric&appid=c27c727042da1e145bf14c1d47031e5d&cnt=5"
     )
       .then((response) => response.json())
       .then((data) => this.showData(data))
       // data.
       .catch(function (error) {
         console.log(error);
-      });
+      });    
+
   },
   showData: function (data) {
-    const date = new Date();
-    console.log(date);
+    const date = new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    console.log(data);
     const { name } = data;
     const { temp, humidity } = data.main;
-    const { speed } = data.wind; 
-    const { icon, description } =data.weather[0];
-    console.log(name, temp, humidity,speed,description,icon);
-    console.log(data);
+    const { speed } = data.wind;
+    const { icon, description } = data.weather[0];
+    console.log(name, temp, humidity, speed, description, icon);
 
     // const desc = description.charAt(0).toUpperCase()+ word.slice(1);
 
-    document.querySelector(".city").innerHTML = `Weather in ${name} `; 
+    document.querySelector(".city").innerHTML = `Weather in ${name} `;
     document.querySelector(".temp").innerHTML = `Temperature: ${temp} C `;
     document.querySelector(".description").innerHTML = `${description} `;
     document.querySelector(".wind").innerHTML = `Wind Speed: ${speed} m/s`;
     document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`;
-    document.querySelector(".icon").src = `http://openweathermap.org/img/w/${icon}.png`;
-    document.querySelector(".icon").alt = `${description}`
+    document.querySelector(
+      ".icon"
+    ).src = `http://openweathermap.org/img/w/${icon}.png`;
+    document.querySelector(".icon").alt = `${description}`;
     document.querySelector(".date").innerHTML = date;
   },
-  submit : function () {
+  submit: function () {
     this.getWeather(document.querySelector("#citySearch").value);
-  }
+    // this.showCast(document.querySelector("#citySearch").value);
+  },
 };
-document.querySelector(".search").addEventListener("click", function(event){
+
+const weatherCast = {
+  forecast: function (city) {
+    fetch(
+      // "https://api.openweathermap.org/data/2.5/forecast?q=brooklyn&units=metric&appid=c27c727042da1e145bf14c1d47031e5d&cnt=40"
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&units=metric&appid=c27c727042da1e145bf14c1d47031e5d&cnt=30"
+    )
+      .then((response) => response.json())
+      .then((data) => this.showCast(data))
+
+      // .then((data) => console.log(data))
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  showCast: function (data) {
+    console.log(data);
+    const { dt_txt: day2 } = data.list[5];
+    const { dt_txt: day3 } = data.list[13];
+    const { dt_txt: day4 } = data.list[21];
+    const { dt_txt: day5 } = data.list[29];
+
+    const { temp: temp2 } = data.list[5].main;
+    const { temp: temp3 } = data.list[13].main;
+    const { temp: temp4 } = data.list[21].main;
+    const { temp: temp5 } = data.list[29].main;
+
+    const { humidity: humidity2 } = data.list[5].main;
+    const { humidity: humidity3 } = data.list[13].main;
+    const { humidity: humidity4 } = data.list[21].main;
+    const { humidity: humidity5 } = data.list[29].main;
+
+    const { speed: wind2 } = data.list[5].wind;
+    const { speed: wind3 } = data.list[13].wind;
+    const { speed: wind4 } = data.list[21].wind;
+    const { speed: wind5 } = data.list[29].wind;
+
+    const { description: description2, icon: icon2 } = data.list[5].weather[0];
+    const { description: description3, icon: icon3 } = data.list[13].weather[0];
+    const { description: description4, icon: icon4 } = data.list[21].weather[0];
+    const { description: description5, icon: icon5 } = data.list[29].weather[0];
+
+    console.log(day2, day3, day4, day5);
+
+    console.log(temp2, temp3, temp4, temp5);
+
+    console.log(
+      description2,
+      description3,
+      description4,
+      description5,
+      icon2,
+      icon3,
+      icon4,
+      icon5
+    );
+
+    console.log(wind2, wind3, wind4, wind5);
+
+    console.log(humidity2, humidity3, humidity4, humidity5);
+
+    document.querySelector(".forecastCity2").innerHTML = `${day2}`;
+    document.querySelector(".forecastCity3").innerHTML = `${day3}`;
+    document.querySelector(".forecastCity4").innerHTML = `${day4}`;
+    document.querySelector(".forecastCity5").innerHTML = `${day5}`;
+
+    document.querySelector(
+      ".forecastTemp2"
+    ).innerHTML = `Temperature: ${temp2}`;
+    document.querySelector(
+      ".forecastTemp3"
+    ).innerHTML = `Temperature: ${temp3}`;
+    document.querySelector(
+      ".forecastTemp4"
+    ).innerHTML = `Temperature: ${temp4}`;
+    document.querySelector(
+      ".forecastTemp5"
+    ).innerHTML = `Temperature: ${temp5}`;
+
+    document.querySelector(
+      ".forecastDescription2"
+    ).innerHTML = `${description2}`;
+    document.querySelector(
+      ".forecastDescription3"
+    ).innerHTML = `${description3}`;
+    document.querySelector(
+      ".forecastDescription4"
+    ).innerHTML = `${description4}`;
+    document.querySelector(
+      ".forecastDescription5"
+    ).innerHTML = `${description5}`;
+
+    document.querySelector(
+      ".forecastIcon2"
+    ).src = `http://openweathermap.org/img/w/${icon2}.png`;
+    document.querySelector(
+      ".forecastIcon3"
+    ).src = `http://openweathermap.org/img/w/${icon3}.png`;
+    document.querySelector(
+      ".forecastIcon4"
+    ).src = `http://openweathermap.org/img/w/${icon4}.png`;
+    document.querySelector(
+      ".forecastIcon5"
+    ).src = `http://openweathermap.org/img/w/${icon5}.png`;
+
+    document.querySelector(".forecastWind2").innerHTML = `${wind2} m/s`;
+    document.querySelector(".forecastWind3").innerHTML = `${wind3} m/s`;
+    document.querySelector(".forecastWind4").innerHTML = `${wind4} m/s`;
+    document.querySelector(".forecastWind5").innerHTML = `${wind5} m/s`;
+
+    document.querySelector(".forecastHumidity2").innerHTML = `${humidity2}%`;
+    document.querySelector(".forecastHumidity3").innerHTML = `${humidity3}%`;
+    document.querySelector(".forecastHumidity4").innerHTML = `${humidity4}%`;
+    document.querySelector(".forecastHumidity5").innerHTML = `${humidity5}%`;
+  },
+  submit: function () {
+    this.forecast(document.querySelector("#citySearch").value);
+    // this.showCast(document.querySelector("#citySearch").value);
+  },
+};
+
+document.querySelector(".search").addEventListener("click", function (event) {
+  event.preventDefault();
+  weatherCast.submit();
+});
+
+document.querySelector(".search").addEventListener("click", function (event) {
   event.preventDefault();
   weather.submit();
-})
-
-
-
+});
